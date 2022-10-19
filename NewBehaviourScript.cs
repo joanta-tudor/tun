9ea92b5b929b6474bcc2ae4fd8b9f3d90c0b2f15
joanta-tudor <62553012+joanta-tudor@users.noreply.github.com>
2022-10-19 23:31:12 +0300

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+        if (collision.relativeVelocity.magnitude > 0.5)
+            audioSource.Play();
+    }
+}
